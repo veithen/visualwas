1588f13227da34cf39eb455232a3295bf3013374
Andreas Veithen <andreas.veithen@gmail.com>
2013-08-12 19:50:31 +0200

    Need to add bootstrap.jar when loading WebSphere classes

--- loader/src/main/java/com/github/veithen/visualwas/loader/WebSphereRuntimeClassLoader.java
+++ loader/src/main/java/com/github/veithen/visualwas/loader/WebSphereRuntimeClassLoader.java
@@ -2,13 +2,19 @@ package com.github.veithen.visualwas.loader;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 public class WebSphereRuntimeClassLoader extends ClassLoader {
     private final Realm realm;
     
     public WebSphereRuntimeClassLoader(File wasHome, ClassLoader parent) throws IOException {
         super(parent);
-        this.realm = new Realm(wasHome, parent);
+        // Classes in the plugins may depend on bootstrap.jar (mainly for logging). Therefore we
+        // need to set up a class loader with this library and use it as the parent class loader for
+        // the realm. Note that the way this is set up implies that the classes in
+        // bootstrap.jar are not visible through the WebSphereRuntimeClassLoader.
+        this.realm = new Realm(wasHome, new URLClassLoader(new URL[] { new File(wasHome, "lib/bootstrap.jar").toURI().toURL() }, parent));
     }
     
     @Override
