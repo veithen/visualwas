c4e5f488b621376589899052b686d3f8e34a727d
Andreas Veithen <andreas.veithen@gmail.com>
2013-11-16 16:03:59 +0100

    Added some documentation

--- connector/src/main/java/com/github/veithen/visualwas/connector/impl/ConfigurableObjectInputStream.java
+++ connector/src/main/java/com/github/veithen/visualwas/connector/impl/ConfigurableObjectInputStream.java
@@ -5,9 +5,22 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectStreamClass;
 
+/**
+ * {@link ObjectInputStream} subclass that resolves class names using a specified class loader.
+ */
 final class ConfigurableObjectInputStream extends ObjectInputStream {
     private final ClassLoader classLoader;
     
+    /**
+     * Constructor.
+     * 
+     * @param in
+     *            the input stream to read from
+     * @param classLoader
+     *            the class loader to load classes from
+     * @throws IOException
+     *             if an I/O error occurs while reading the stream header
+     */
     ConfigurableObjectInputStream(InputStream in, ClassLoader classLoader) throws IOException {
         super(in);
         this.classLoader = classLoader;
@@ -15,6 +28,7 @@ final class ConfigurableObjectInputStream extends ObjectInputStream {
 
     @Override
     protected Class<?> resolveClass(ObjectStreamClass desc) throws IOException, ClassNotFoundException {
+        // Note: we can't use ClassLoader#loadClass here because the name may denote an array class
         return Class.forName(desc.getName(), false, classLoader);
     }
 }
