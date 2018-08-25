ead9dd7739059dd6959ed3d513d94000159588b4
Andreas Veithen <andreas.veithen@gmail.com>
2018-08-25 13:21:20 +0100

    Add test case for MXBean authorization

--- websphere-plugin/src/test/java/com/github/veithen/visualwas/mxbeans/MXBeansITCase.java
+++ websphere-plugin/src/test/java/com/github/veithen/visualwas/mxbeans/MXBeansITCase.java
@@ -24,6 +24,7 @@ package com.github.veithen.visualwas.mxbeans;
 import static com.google.common.truth.Truth.assertThat;
 
 import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
 import java.lang.management.RuntimeMXBean;
 import java.net.Proxy;
 import java.util.HashMap;
@@ -42,19 +43,42 @@ import com.github.veithen.visualwas.jmx.soap.SOAPJMXConnector;
 import com.github.veithen.visualwas.x509.PromiscuousTrustManager;
 
 public class MXBeansITCase {
-    @Test
-    public void test() throws Exception {
+    private interface Action<T> {
+        void run(T mxbean) throws Exception;
+    }
+    
+    private static <T> void run(String role, String objectName, Class<T> interfaceClass, Action<T> action) throws Exception {
         Map<String,Object> env = new HashMap<String,Object>();
         env.put(JMXConnectorFactory.PROTOCOL_PROVIDER_PACKAGES, "com.github.veithen.visualwas.jmx");
         env.put(SOAPJMXConnector.PROXY, Proxy.NO_PROXY);
-        env.put(JMXConnector.CREDENTIALS, new String[] { "monitor", "changeme" });
+        env.put(JMXConnector.CREDENTIALS, new String[] { role, "changeme" });
         env.put(SOAPJMXConnector.TRUST_MANAGER, PromiscuousTrustManager.INSTANCE);
         JMXServiceURL url = new JMXServiceURL("soap", "localhost", Integer.parseInt(System.getProperty("was.soapPort")));
         JMXConnector connector = JMXConnectorFactory.connect(url, env);
         MBeanServerConnection connection = connector.getMBeanServerConnection();
-        RuntimeMXBean runtimeMXBean = JMX.newMXBeanProxy(connection, new ObjectName(ManagementFactory.RUNTIME_MXBEAN_NAME), RuntimeMXBean.class);
-        Map<String,String> systemProperties = runtimeMXBean.getSystemProperties();
-        assertThat(systemProperties).containsEntry("java.util.logging.manager", "com.ibm.ws.bootstrap.WsLogManager");
+        action.run(JMX.newMXBeanProxy(connection, new ObjectName(objectName), interfaceClass));
         connector.close();
+    }
+    
+    @Test
+    public void testRuntimeMXBean() throws Exception {
+        run("monitor", ManagementFactory.RUNTIME_MXBEAN_NAME, RuntimeMXBean.class, (runtimeMXBean) -> {
+            Map<String,String> systemProperties = runtimeMXBean.getSystemProperties();
+            assertThat(systemProperties).containsEntry("java.util.logging.manager", "com.ibm.ws.bootstrap.WsLogManager");
+        });
+    }
+    
+    @Test(expected=SecurityException.class)
+    public void testAccessDenied() throws Exception {
+        run("monitor", ManagementFactory.MEMORY_MXBEAN_NAME, MemoryMXBean.class, (memoryMXBean) -> {
+            memoryMXBean.gc();
+        });
+    }
+    
+    @Test
+    public void testAccessGranted() throws Exception {
+        run("operator", ManagementFactory.MEMORY_MXBEAN_NAME, MemoryMXBean.class, (memoryMXBean) -> {
+            memoryMXBean.gc();
+        });
     }
 }
